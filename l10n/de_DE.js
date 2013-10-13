75dc3ce2c4fb97c88eef0feebb40bb231eb20d33
hahnjo <hahnjo@hahnjo.de>
2013-10-13 12:27:16 +0200

    l10n: German translation

---
+++ l10n/de_DE.js
@@ -0,0 +1,11 @@
+// http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes
+var htmlLang = "de";
+var l10n = {
+	"Statistics for Facebook Messages": "Statistiken für die Facebook-Nachrichten",
+	"We are sorry, but this app isn't working for you. We are relying a new API that Facebook has not made available publically yet :-(": "Es tut uns Leid, aber diese App funktioniert nicht für dich. Wir verwenden eine neue API, die Facebook noch nicht für alle verfügbar gemacht hat :-(",
+	"Title": "Titel",
+	"Last message": "Letzte Nachricht",
+	"Count": "Anzahl",
+	"First message": "Erste Nachricht",
+	"Messages per day": "Nachrichten pro Tag",
+};
