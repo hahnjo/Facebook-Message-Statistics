8c8151adb13b03b9d5a54d937f9b850149d4f16d
hahnjo <hahnjo@hahnjo.de>
2013-10-13 14:51:34 +0200

    l10n: Update German translation

--- l10n/de_DE.js
+++ l10n/de_DE.js
@@ -3,6 +3,9 @@ var htmlLang = "de";
 var l10n = {
 	"Statistics for Facebook Messages": "Statistiken für die Facebook-Nachrichten",
 	"We are sorry, but this app isn't working for you. We are relying a new API that Facebook has not made available publically yet :-(": "Es tut uns Leid, aber diese App funktioniert nicht für dich. Wir verwenden eine neue API, die Facebook noch nicht für alle verfügbar gemacht hat :-(",
+	"All in all you wrote": "Insgesamt hast du",
+	"messages with your": "Nachrichten mit deinen",
+	"friends": "Freunden geschrieben",
 	"Title": "Titel",
 	"Last message": "Letzte Nachricht",
 	"Count": "Anzahl",
